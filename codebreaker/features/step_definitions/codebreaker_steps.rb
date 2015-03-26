59df3227f319b952bdc466bf3b135d5dada8b76b
mikelangston <mikemac8203@gmail.com>
2015-03-25 21:08:44 -0400

    added to codebreaker

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -6,10 +6,10 @@ class Output
   def puts(message)
     messages << message
   end
+end
 
-  def output
-    @output ||= Output.new
-  end
+def output
+  @output ||= Output.new
 end
 
 Given(/^I am not yet playing$/) do 
